61aaf40df6e89840245afe3f456c23a78a83f66b
Alessia Bellisario <bellisario.alessia@gmail.com>
2014-06-03 12:36:38 -0400

    Returns name and birthyear

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,9 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "Hi #{name}!"
+
+puts "How old are you?"
+age = gets.chomp
+birthyear = (2014) - (age.to_i)
+
+puts "Cool, #{name}! You were born in #{birthyear}!"
